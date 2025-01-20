5620120f4a55c20af97c39dca2f626aca13572bd
IsraelAbra1 <israel.kugel10@gmail.com>
2025-01-20 22:54:35 +0200

    check

--- app/src/main/java/com/example/serviceforegroundnotification/MainActivity.java
+++ app/src/main/java/com/example/serviceforegroundnotification/MainActivity.java
@@ -14,7 +14,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main); // -- abcccccccc
+        setContentView(R.layout.activity_main); // -- abcccccccc 1111111111111111111
 
 
         btnPush = (Button)findViewById(R.id.btnPush);
